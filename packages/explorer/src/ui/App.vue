7c65c5384db0f12b39a0634b226660d7f64c5336
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2022-07-15 10:04:33 -0400

    fix: new directory structure

--- packages/explorer/src/ui/App.vue
+++ packages/explorer/src/ui/App.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "@/ui/components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/ui/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -21,7 +27,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 </template>
 
 <style>
-@import '@/assets/base.css';
+@import "@/ui/assets/base.css";
 
 #app {
   max-width: 1280px;
--- packages/explorer/src/ui/views/HomeView.vue
+++ packages/explorer/src/ui/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import TheWelcome from "@/ui/components/TheWelcome.vue";
 </script>
 
 <template>
