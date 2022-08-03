07411ff9dae21506fbced3a7a7abcf71e9137882
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2022-08-03 13:59:32 -0400

    feat: implement method

--- packages/explorer/src/domain/objects/entities/coordUtils.ts
+++ packages/explorer/src/domain/objects/entities/coordUtils.ts
@@ -1,3 +1,17 @@
-export const degToHms = (deg: number): string => {
-  throw new Error("Method not implemented");
+export const decToHms = (deg: number): string => {
+  const sign = deg < 0 ? "-" : "+";
+  let dec = Math.abs(deg);
+  deg = Math.floor(dec);
+  const decM = Math.abs(Math.floor((dec - deg) * 60));
+  const decS = ((Math.abs((dec - deg) * 60) - decM) * 60).toFixed(2);
+  return `${sign}${deg}:${decM}:${decS}`;
+};
+
+export const raToHms = (deg: number): string => {
+  const sign = deg < 0 ? "-" : "\xA0";
+  const ra = Math.abs(deg);
+  const raH = Math.floor(ra / 15);
+  const raM = Math.floor((ra / 15 - raH) * 60);
+  const raS = (((ra / 15 - raH) * 60 - raM) * 60).toFixed(3);
+  return `${sign}${raH}:${raM}:${raS}`;
 };
