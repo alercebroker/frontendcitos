a3162b449037d256041e645ccb1c99add99efec5
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2022-08-03 14:00:42 -0400

    feat: implementation of object repository

--- packages/explorer/src/app/object/adapters/objectRepository.ts
+++ packages/explorer/src/app/object/adapters/objectRepository.ts
@@ -1,19 +1,85 @@
-import type {
-  ObjectEntity,
-  ObjectListEntity,
-  ObjectListFilters,
+import {
+  raToHms,
+  mjdToGreg,
+  type ObjectEntity,
+  type ObjectListEntity,
+  type ObjectListFilters,
+  decToHms,
 } from "@/domain/objects/entities";
 import type { ObjectRepository } from "@/domain/objects/ports";
-import type { HttpError, ParseError } from "@alercebroker/http-client";
-import type { Result } from "neverthrow";
+import {
+  AlertsClient,
+  isHttpError,
+  isParseError,
+} from "@alercebroker/http-client";
+import type {
+  listObjectResponse,
+  objectListItem,
+  Parser,
+  HttpError,
+  ParseError,
+} from "@alercebroker/http-client/build/main/types";
+import { err, ok, type Result } from "neverthrow";
 
 export const objectRepository: ObjectRepository = {
-  getObject: (): ObjectEntity => {
-    throw new Error("Not Implemented");
-  },
-  getObjects: (
-    filters: ObjectListFilters
-  ): Result<ObjectListEntity, ParseError | HttpError> => {
-    throw new Error("Not Implemented");
+  getObject,
+  getObjects,
+};
+
+export const objectListParser: Parser<listObjectResponse, ObjectListEntity> = {
+  parseTo: (response: listObjectResponse): ObjectListEntity => {
+    const result: ObjectListEntity = {
+      total: response.total,
+      page: response.page,
+      next: response.next,
+      hasNext: response.has_next,
+      prev: response.prev,
+      hasPrev: response.has_prev,
+      items: parseItems(response.items),
+    };
+    return result;
   },
 };
+
+function parseItems(items: objectListItem[]): ObjectEntity[] {
+  return items.map((item) => {
+    const objectEntity: ObjectEntity = {
+      aid: item.aid,
+      ra: item.meanra,
+      dec: item.meandec,
+      firstmjd: item.firstmjd,
+      firstGreg: mjdToGreg(item.firstmjd),
+      lastmjd: item.lastmjd,
+      lastGreg: mjdToGreg(item.lastmjd),
+      raHms: raToHms(item.meanra),
+      decHms: decToHms(item.meandec),
+    };
+    return objectEntity;
+  });
+}
+
+async function getObjects(
+  filters: ObjectListFilters
+): Promise<Result<ObjectListEntity, ParseError | HttpError>> {
+  try {
+    const result = await AlertsClient.queryObjects<ObjectListEntity>(
+      filters,
+      objectListParser
+    );
+    return ok(result);
+  } catch (error) {
+    if (error instanceof Error) {
+      if (isHttpError(error) || isParseError(error)) {
+        return err(error);
+      }
+    }
+    // Unknown error, just rethrow
+    throw error;
+  }
+}
+
+async function getObject(
+  id: string
+): Promise<Result<ObjectEntity, ParseError | HttpError>> {
+  throw new Error("Not Implemented");
+}
