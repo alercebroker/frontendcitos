9774e5c9fda9ab01d655266ae5eab101d873b8f5
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2022-08-04 13:14:51 -0400

    refactor: delete container

--- packages/explorer/src/ui/main.ts
+++ packages/explorer/src/ui/main.ts
@@ -1,4 +1,3 @@
-import { containerBuilder } from "@/common/container/container";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { Quasar, Dark } from "quasar";
@@ -10,7 +9,6 @@ import "@quasar/extras/material-icons-outlined/material-icons-outlined.css";
 import "quasar/src/css/index.sass";
 
 const createdPinia = createPinia();
-containerBuilder();
 import App from "./App.vue";
 import router from "./router";
 const app = createApp(App);
