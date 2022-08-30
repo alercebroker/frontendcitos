b5b2cbc3035feb8aff14538662af4b701e8fe11c
Pedro <pegallardo@inf.udec.cl>
2022-08-30 15:45:41 -0400

    fix(vitest): hunter wont have integration tests

--- packages/hunter/vitest.config.ts
+++ packages/hunter/vitest.config.ts
@@ -3,20 +3,12 @@ import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
 import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
-const include = (mode: string | undefined) => {
-  if (mode === "integration") {
-    return ["**/*.integration.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"];
-  } else {
-    return ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"];
-  }
-};
-
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
     environment: "jsdom",
     globals: false,
-    include: include(process.env.TEST_MODE),
+    include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
   },
   plugins: [
     vue({
