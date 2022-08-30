7682035e6b8efdf282660bcf1b8b66b964336581
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2022-08-30 12:36:15 -0400

    feat(explorer): Add object details view layout

---
+++ packages/explorer/src/ui/layouts/ObjectDetails.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+<template>
+  <q-layout>
+    <q-header>
+      <div class="row no-shadow justify-end toolbar-space">
+        <q-toolbar class="col-4 bg-primary">
+          <q-avatar>
+            <img
+              src="https://alerce-static.s3.amazonaws.com/logos/footerAlerceWhite.png"
+            />
+          </q-avatar>
+
+          <q-toolbar-title>ALeRCE Explorer</q-toolbar-title>
+          <q-btn flat round dense icon="menu" class="q-mr-sm" />
+        </q-toolbar>
+      </div>
+    </q-header>
+
+    <q-page-container>
+      <RouterView />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<style scoped lang="sass">
+.toolbar-space
+    background-color: $light-page
+</style>
--- packages/explorer/src/ui/router/index.ts
+++ packages/explorer/src/ui/router/index.ts
@@ -1,27 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Search from '@ui/layouts/Search.vue'
-import SearchHome from '@ui/views/SearchHome.vue'
-import SearchResults from '@ui/views/SearchResults.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Search from "@ui/layouts/Search.vue";
+import SearchHome from "@ui/views/SearchHome.vue";
+import SearchResults from "@ui/views/SearchResults.vue";
+import ObjectDetails from "@ui/layouts/ObjectDetails.vue";
+import ObjectDetailsView from "@ui/views/ObjectDetailsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'search',
+      path: "/",
+      name: "search",
       component: Search,
       children: [
         {
-          path: '/',
-          component: SearchHome
+          path: "/",
+          component: SearchHome,
         },
         {
-          path: 'results',
-          component: SearchResults
-        }
-      ]
+          path: "results",
+          component: SearchResults,
+        },
+      ],
     },
-  ]
-})
+    {
+      path: "/object/:objectId",
+      name: "object-details",
+      component: ObjectDetails,
+      children: [
+        {
+          path: "",
+          component: ObjectDetailsView,
+          name: "object",
+        },
+      ],
+    },
+  ],
+});
 
-export default router
+export default router;
