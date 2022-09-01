289c82c2e487644cdff24a5d043bc5450d3c362b
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2022-09-01 16:15:20 -0400

    refactor: make routes a constant

--- packages/explorer/src/ui/router/index.ts
+++ packages/explorer/src/ui/router/index.ts
@@ -1,37 +1,40 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+export const routes = [
+  {
+    path: "/",
+    name: "search",
+    component: () => import("@ui/layouts/Search.vue"),
+    children: [
+      {
+        path: "/",
+        component: () => import("@ui/views/SearchHome.vue"),
+        name: "search",
+      },
+      {
+        path: "results",
+        name: "results",
+        component: () => import("@ui/views/SearchResults.vue"),
+      },
+    ],
+  },
+  {
+    path: "/object/:objectId",
+    name: "object-details",
+    component: () => import("@ui/layouts/ObjectDetails.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("@ui/views/ObjectDetailsView.vue"),
+        name: "object",
+      },
+    ],
+  },
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "search",
-      component: () => import("@ui/layouts/Search.vue"),
-      children: [
-        {
-          path: "/",
-          component: () => import("@ui/views/SearchHome.vue"),
-        },
-        {
-          path: "results",
-          name: "results",
-          component: () => import("@ui/views/SearchResults.vue"),
-        },
-      ],
-    },
-    {
-      path: "/object/:objectId",
-      name: "object-details",
-      component: () => import("@ui/layouts/ObjectDetails.vue"),
-      children: [
-        {
-          path: "",
-          component: () => import("@ui/views/ObjectDetailsView.vue"),
-          name: "object",
-        },
-      ],
-    },
-  ],
+  routes,
 });
 
 export default router;
