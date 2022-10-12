e734052a456a217a824874cbdb92bcfe43178138
Alex Alvarez Toledo <otajla@hotmail.com>
2022-10-12 03:12:07 -0300

    resolved mock store

--- packages/explorer/src/ui/stores/object_details/__mocks__/di.ts
+++ packages/explorer/src/ui/stores/object_details/__mocks__/di.ts
@@ -1,12 +1,12 @@
 import {
-  getObjectListUseCase,
-  convertMjdUseCase,
-  convertGregUseCase,
+  getObjectSingleUseCase,
+  getObjectLightCurveUseCase
 } from "@/app/object/use-cases/__mocks__";
 import { objectDetailsStore } from "../store";
 
 const searchStoreResolved = objectDetailsStore(
-  
+  getObjectSingleUseCase,
+  getObjectLightCurveUseCase  
 );
 
 export { searchStoreResolved as useSearchStore };
