9220ba04e76b8f8d6b7079b5b2bb2536e04dc0d0
Alex Alvarez Toledo <otajla@hotmail.com>
2022-10-12 02:49:48 -0300

    feat: initial development for the store

---
+++ packages/explorer/src/ui/stores/object_details/__mocks__/di.ts
@@ -0,0 +1,12 @@
+import {
+  getObjectListUseCase,
+  convertMjdUseCase,
+  convertGregUseCase,
+} from "@/app/object/use-cases/__mocks__";
+import { objectDetailsStore } from "../store";
+
+const searchStoreResolved = objectDetailsStore(
+  
+);
+
+export { searchStoreResolved as useSearchStore };
